fc4bf5b86eaabb719b7cdf19f6647739590d874b
Michael Behr <mkbehr@gmail.com>
2015-08-23 14:50:25 -0400

    Make GL draw something to the screen

--- screen.py
+++ screen.py
@@ -81,8 +81,8 @@ class Screen(object):
 
         vertexShaderSrc = """#version 330
 
-        in uvec2 xy;
-        in uvec3 v_tuv;
+        in vec2 xy;
+        in vec3 v_tuv;
         in uint v_palette_n;
 
         out vec3 f_uvt;
@@ -90,7 +90,7 @@ class Screen(object):
 
         void main()
         {
-          gl_Position = vec4(xy.x / 256.0, xy.y / 240.0, 0.0, 1.0);
+          gl_Position = vec4((float(xy.x) / 16.0) - 1, (float(xy.y) / 15.0) - 1, 0.0, 1.0);
           f_uvt = vec3(v_tuv.y/8.0, v_tuv.z/8.0, v_tuv.x);
           //f_palette_n = v_palette_n;
         }"""
@@ -119,6 +119,7 @@ class Screen(object):
         // DEBUG:
           float greyShade = localPaletteIndex / 4.0;
           outColor = vec4(greyShade, greyShade, greyShade, 1.0);
+          outColor = vec4(0.0, 0.0, 1.0, 1.0);
         }"""
         fragmentShader = shaders.compileShader(fragmentShaderSrc, GL_FRAGMENT_SHADER)
         self.shader = shaders.compileProgram(vertexShader, fragmentShader)
@@ -131,10 +132,10 @@ class Screen(object):
         self.lastBgPalette = None
 
         self.bgVbo = glGenBuffers(1)
-        
+
         self.bgPtabName = glGenTextures(1)
 
-        # TODO bind global palette 
+        # TODO bind global palette
 
         # self.bgTextureNames = [
         #     # I don't really know what I'm doing here
@@ -200,7 +201,7 @@ class Screen(object):
         glBindBuffer(GL_ARRAY_BUFFER, self.bgVbo)
 
         self.maintainPaletteTable()
-        
+
         (bg_r, bg_g, bg_b) = palette.PALETTE[self.ppu.universalBg]
         (bg_r, bg_g, bg_b) = (128,128,128) # DEBUG
         glClearColor(bg_r / 255.0, bg_g / 255.0, bg_b / 255.0, 1.0)
@@ -211,12 +212,16 @@ class Screen(object):
 
         # TODO get rid of some of these magic numbers
         vertexList = [0 for x in range(TILE_COLUMNS * TILE_ROWS * 6 * 6)]
+        # Note: x and y coordinates are in increments of tiles, so
+        # they range from 0 to 32. This is a dumb hack because the
+        # vertex shader was doing dumb things when I told it to divide
+        # by 256, for reasons I don't understand.
         for x in xrange(TILE_COLUMNS):
             for y in xrange(TILE_ROWS):
-                x_left = x * 8
-                x_right = (x+1) * 8
-                y_bottom = (SCREEN_HEIGHT - y*8 - 8)
-                y_top = (SCREEN_HEIGHT - y*8)
+                x_left = x
+                x_right = x+1
+                y_bottom = (TILE_ROWS - y - 1)
+                y_top = (TILE_ROWS - y)
                 tile = 0 # TODO: determined by the nametable
                 # TODO see if these uv coordinates are right
                 u_left = 0
@@ -250,13 +255,13 @@ class Screen(object):
 
         # TODO point attributes into that big buffer
         xyOffset = ctypes.c_void_p(0)
-        glVertexAttribPointer(self.xyAttrib, 2, GL_FLOAT, GL_FALSE, stride, xyOffset)
+        glVertexAttribPointer(self.xyAttrib, 2, GL_UNSIGNED_BYTE, GL_FALSE, stride, xyOffset)
         glEnableVertexAttribArray(self.xyAttrib)
         tuvOffset = ctypes.c_void_p(2 * ctypes.sizeof(ctypes.c_ubyte))
-        glVertexAttribPointer(self.tuvAttrib, 3, GL_FLOAT, GL_FALSE, stride, tuvOffset)
+        glVertexAttribPointer(self.tuvAttrib, 3, GL_UNSIGNED_BYTE, GL_FALSE, stride, tuvOffset)
         glEnableVertexAttribArray(self.tuvAttrib)
         # paletteNOffset = ctypes.c_void_p(5 * ctypes.sizeof(ctypes.c_ubyte))
-        # glVertexAttribPointer(self.paletteNAttrib, 1, GL_FLOAT, GL_FALSE, stride, paletteNOffset)
+        # glVertexAttribIPointer(self.paletteNAttrib, 1, GL_FLOAT, stride, paletteNOffset)
         # glEnableVertexAttribArray(self.paletteNAttrib)
 
         # TODO point uniform arguments
@@ -275,11 +280,11 @@ class Screen(object):
         glDrawArrays(GL_TRIANGLES, 0, N_VERTICES)
 
         glfw.swap_buffers(self.window)
-        
+
     def maintainPaletteTable(self):
         # Note: only call this when self.bgVbo is bound to GL_ARRAY_BUFFER... maybe?
         if self.lastBgPalette != self.ppu.bgPatternTableAddr:
-            
+
             self.patternTable = self.ppu.dumpPtab(self.ppu.bgPatternTableAddr)
             glBindTexture(GL_TEXTURE_2D_ARRAY, self.bgPtabName)
             glActiveTexture(BG_PATTERN_TABLE_TEXTURE)
